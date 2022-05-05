db41a10fd85f34719c1d0f4c416eee7d7e18a992
Nithish131 <104966368+Nithish131@users.noreply.github.com>
2022-05-05 11:41:05 +0530

    Add files via upload

---
+++ day3/learloop.java
@@ -0,0 +1,15 @@
+package week1.day3;
+
+import java.util.Iterator;
+
+public class learloop {
+	public static void main(String[] args) {
+		for (int i = 3; i <=7; i++) {
+			System.out.println(i);
+			
+		}
+			
+		}
+	}
+
+
