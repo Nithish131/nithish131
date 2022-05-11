4806c5a7b17ce24e627b3720b845b4060623d294
Nithish131 <104966368+Nithish131@users.noreply.github.com>
2022-05-11 14:52:22 +0530

    Add files via upload

---
+++ day1/Firstprogram.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Firstprogram {
+	//public class FirstProgram {
+		public static void main(String[] args) {
+			String name="TestLeaf";
+			System.out.println(name);
+		}
+
+	}
+
+
