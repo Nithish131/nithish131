efdcbcae32a047712b07dcb13c0c2dd42331c056
Nithish131 <104966368+Nithish131@users.noreply.github.com>
2022-05-11 14:54:14 +0530

    Add files via upload

---
+++ day2/ClassMobile.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class ClassMobile {
+	public void sendMessage() {
+		System.out.println("send message");
+	}
+	public void shareDocument() {
+		System.out.println("share document");
+	}
+	public void call() {
+		System.out.println("make call");
+	}
+
+
+
+}
---
+++ day2/Variable.java
@@ -0,0 +1,9 @@
+package week1.day2;
+
+public class Variable {
+	String text="TestLeaf";
+	int a;
+	
+}
+
+
